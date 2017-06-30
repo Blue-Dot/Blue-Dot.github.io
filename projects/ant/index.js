78d1f5cc4aa78296a665bc498cdd556b73d9f069
Blue-Dot <Blue-Dot@users.noreply.github.com>
2017-06-30 19:02:52 +0100

    Create index.js

---
+++ projects/ant/index.js
@@ -0,0 +1,10 @@
+/// write to file
+var txtFile = "c:/test.txt";
+var file = new File(txtFile);
+var str = "My string of text";
+
+file.open("w"); // open file with write access
+file.writeln("First line of text");
+file.writeln("Second line of text " + str);
+file.write(str);
+file.close();
