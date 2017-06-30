c8adf59c4b0ef070d23102958c7b8c82ea85c323
Blue-Dot <Blue-Dot@users.noreply.github.com>
2017-06-30 20:12:11 +0100

    Update index.php

--- projects/ant/index.php
+++ projects/ant/index.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
 <html>
-  <head>
-    <title>Page Title</title>
-    <script language = "javascript" src = "index.js"></script>
-  </head>
-  <body>
-    
-    <h1>My First Heading</h1>
-    <p>My first paragraph.</p>
-    
-    <?
-      //$myfile = fopen("testfile.txt", "w")
-      echo "hello";
-    ?>
-
-  </body>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
 </html>
