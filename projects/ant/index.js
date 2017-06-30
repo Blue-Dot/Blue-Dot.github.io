1191daa5ef334f1196c97cb717480250982fad9f
Blue-Dot <Blue-Dot@users.noreply.github.com>
2017-06-30 20:00:38 +0100

    Update index.js

--- projects/ant/index.js
+++ projects/ant/index.js
@@ -1,7 +1,1 @@
-/// write to file
-//set fso = CreateObject("Scripting.FileSystemObject"); 
-s = fso.CreateTextFile("C:\test.txt", True);
-s.writeline("HI");
-s.writeline("Bye");
-s.writeline("-----------------------------");
-s.Close();
+
